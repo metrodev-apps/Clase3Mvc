4edd2b9b8c28c97e314a4e644bf2f8fe8019daa6
Moises Rivas <moises.rivas@metrodev.com.ar>
2021-12-20 20:19:06 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Clase3Mvc.Models;
+
+namespace Clase3Mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly List<Team> _teams;
+
+    public HomeController(ILogger<HomeController> logger)
+    {        
+        _logger = logger;
+        _teams = new List<Team>();
+    }
+
+    public IActionResult Index()
+    {        
+        return View();
+    }
+    
+    public IActionResult Teams()
+    {
+        _teams.Add(new Team(10, "Barcelona"));
+        _teams.Add(new Team(12, "Real Madrid"));
+        _teams.Add(new Team(13, "Atletico de Madrid"));
+        return View(_teams);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,15 @@
+namespace Clase3Mvc.Models;
+
+public class Team
+{
+    public Team(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set;}
+
+    
+}
